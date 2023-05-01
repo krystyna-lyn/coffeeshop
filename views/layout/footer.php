68d3690d581e4b8024254e00edc08ddb448e3575
krystyna-lyn <krystyna_kh@gmx.de>
2023-05-01 23:17:14 +0200

    update styles

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,49 +1,66 @@
 </div>
 </div>
 
-<?php 
+<?php
 require_once 'blog.php';
 require_once 'services.php';
 require_once 'contact.php';
 ?>
 
-    <!-- Footer Section -->
-    <section class="footer">
+<!-- Footer Section -->
+<section class="footer">
 
 
-        <div class="container">
-            <div class="footer-box">
-                <h3>about us </h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non laborum nisi id amet sit? Cum, rem?
-                    Nam, in at. Officiis?</p>
-                <p>Copyrights &copy; <?= date('Y') ?> All rights reserved By KrysLyn</p>
+    <div class="container">
+        <div class="footer-box">
+            <h3>about us </h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non laborum nisi id amet sit? Cum, rem?
+                Nam, in at. Officiis?</p>
+            <p>Copyrights &copy; <?= date('Y') ?> All rights reserved By KrysLyn</p>
 
-            </div>
-            <div class="footer-box">
-                <h3>Newsletter</h3>
-                <p>Stay updaet with our latests</p>
-                <form>
-                    <input type="email" name="email" placeholder="krys@gmail.com">
-                    <button type="submit"><i class="fa-solid fa-right-long"></i></button>
-                </form>
-            </div>
-            <div class="footer-box">
-                <h3>Follow Us</h3>
-                <p>Connect With Us On:</p>
-                <div>
-                    <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
-                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
-                </div>
-				
-
-            </div>
         </div>
-    </section>
-    <!-- Footer Section End -->
+        <div class="footer-box">
+            <h3>Newsletter</h3>
+            <p>Stay updaet with our latests</p>
+            <form>
+                <input type="email" name="email" placeholder="krys@gmail.com">
+                <button type="submit"><i class="fa-solid fa-right-long"></i></button>
+            </form>
+        </div>
+        <div class="footer-box">
+            <h3>Follow Us</h3>
+            <p>Connect With Us On:</p>
+            <div>
+                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+            </div>
 
-				
-		
-		</div>
-	</body>
+
+        </div>
+    </div>
+</section>
+<!-- Footer Section End -->
+
+</div>
+
+<script>
+    let menu = document.getElementById("nav");
+    let open = document.getElementById("menu-btn");
+    let close = document.getElementById("close");
+
+    function openmenu() {
+        menu.style.left = "0";
+        open.style.display = "none";
+        close.style.display = "block";
+    }
+
+    function closemenu() {
+        menu.style.left = "-100%";
+        open.style.display = "block";
+        close.style.display = "none";
+    }
+</script>
+</body>
+
 </html>
